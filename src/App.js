627cda59140ef2e647b23bc343884dce44f9d63c
rahulkumar215 <rk1617015@gmail.com>
2024-08-14 16:44:54 +0530

    implemented project background

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      We're launching soon Days Hours Minutes Seconds
-      <div class="attribution">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Your Name Here</a>.
-      </div>
-    </>
-  );
+  return <>We're launching soon Days Hours Minutes Seconds</>;
 }
 
 export default App;
