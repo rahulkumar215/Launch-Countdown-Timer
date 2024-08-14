7732763022fcc4ca27e33ddf582ec433e8bcd0a9
rahulkumar215 <rk1617015@gmail.com>
2024-08-14 18:15:00 +0530

    started working on flip component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
 
 function App() {
-  return <>We're launching soon Days Hours Minutes Seconds</>;
+  // return <>We're launching soon Days Hours Minutes Seconds</>;
+  return (
+    <div className="container">
+      <div className="">
+        <div className="digit timer">
+          <div className="card back">
+            <div className="top">0</div>
+            <div className="bottom">
+              <div className="inner">0</div>
+            </div>
+          </div>
+          <div className="card active">
+            <div className="front">
+              <div className="top">0</div>
+            </div>
+            <div className="back">
+              <div className="bottom">
+                <div className="inner">0</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
